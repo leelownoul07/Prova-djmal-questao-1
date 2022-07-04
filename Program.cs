545d6acabfbfe415d7669e5f0dd99c26f7ad7c26
Jaime <Jaime@DESKTOP-BVSIG8L>
2022-07-04 12:17:29 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double notat, nota1, nota2, final;
+           string nome;
+           Console.Write("escreva nome do aluno: ");
+           nome = Console.ReadLine();
+           Console.Write("nota do trabalho: ");
+           notat = Convert.ToDouble(Console.ReadLine());
+           Console.Write("digite nota 1:  ");
+           nota1 = Convert.ToDouble(Console.ReadLine());
+           Console.Write("digite nota 2:  ");
+           nota2 = Convert.ToDouble(Console.ReadLine());
+           final = (notat + nota1 + nota2) / 3;
+
+            if (final >= 8.5)
+            {
+                Console.Write("Sua nota foi A");
+            }
+            else if (final >= 7)
+            {
+                Console.Write("Sua nota foi B ");
+            }
+            else if (final >= 6)
+            {
+                Console.Write("Sua nota foi C ");
+            }
+            else if (final >= 0.1)
+            {
+                Console.Write("Sua nota foi D ");
+            }
+            else
+            {
+                Console.Write("Sua nota foi E ");
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
